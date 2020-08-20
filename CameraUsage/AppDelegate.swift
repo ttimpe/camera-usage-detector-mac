bfaf326a99e51484dcef744e9a9aef1f05454fff
Tobias Timpe <tobias.timpe@hunter.de>
2020-08-20 12:46:54 +0200

    Simple Camera usage checker for presence webhooks

--- CameraUsage/AppDelegate.swift
+++ CameraUsage/AppDelegate.swift
@@ -7,21 +7,44 @@
 //
 
 import Cocoa
+import CoreImage
+import CoreMedia
+import CoreMediaIO
+
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
+    let cameraUsageController = CameraUsageController()
 
-
+    @IBOutlet weak var updateURLField: NSTextField!
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+       
+        cameraUsageController.startUpdating()
+        
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+        cameraUsageController.stopUpdating()
     }
 
-
+    @IBAction func showPreferencesWindow(_ sender: Any) {
+        self.window.setIsVisible(true)
+        self.window.makeKey()
+        if let updateURLString = UserDefaults.standard.url(forKey: "updateURL")?.absoluteString {
+            self.updateURLField.stringValue = updateURLString
+        }
+    }
+    
+    @IBAction func saveUpdateURL(_ sender: Any) {
+        if let url = URL(string: self.updateURLField.stringValue) {
+            UserDefaults.standard.set(url, forKey: "updateURL")
+            UserDefaults.standard.synchronize()
+        }
+    }
 }
 
---
+++ CameraUsage/CameraUsageController.swift
@@ -0,0 +1,45 @@
+//
+//  CameraUsageController.swift
+//  CameraUsage
+//
+//  Created by Tobias Timpe on 20.08.20.
+//  Copyright © 2020 Tobias Timpe. All rights reserved.
+//
+
+import Foundation
+import IsCameraOn
+
+class CameraUsageController {
+    var timer: Timer?
+    var lastCameraState: Bool = false
+    
+    
+
+    
+    func startUpdating() {
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(check), userInfo: nil, repeats: true)
+        timer?.tolerance = 0.2
+    }
+    func stopUpdating() {
+        timer?.invalidate()
+    }
+    
+    @objc func check() {
+        if (self.lastCameraState != isCameraOn()) {
+            // Got update
+            self.lastCameraState = isCameraOn()
+            print("State changed")
+            self.sendUpdateToServer()
+        }
+    }
+    func sendUpdateToServer() {
+        UserDefaults.standard.synchronize()
+        if let url = UserDefaults.standard.url(forKey: "updateURL") {
+            URLSession.shared.dataTask(with: url) { (data, response, error) in
+                print("done updating")
+            }.resume()
+        }
+
+    }
+    
+}
