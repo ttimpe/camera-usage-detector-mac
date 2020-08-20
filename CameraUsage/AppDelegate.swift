3c05da71afc771a456b476d65a0078040ceeb067
Tobias Timpe <tobias.timpe@hunter.de>
2020-08-20 11:39:54 +0200

    Initial Commit

---
+++ CameraUsage/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CameraUsage
+//
+//  Created by Tobias Timpe on 20.08.20.
+//  Copyright © 2020 Tobias Timpe. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
